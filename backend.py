f6815fc6b58ee70d3db461ecd9542feba604b222
marcus <howardmilleriv23@gmail.com>
2024-07-23 11:47:30 -0400

    Finished the application

---
+++ backend.py
@@ -0,0 +1,15 @@
+import requests
+
+API_KEY = "1cb6cb9a91c5d2a7ec492fbd6cbc7fc6"
+
+def get_data(place, forecast_days):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    nr_value = 8 * forecast_days
+    filtered_data = filtered_data[:nr_value]
+    return filtered_data
+
+if __name__ == "__main__":
+    print(get_data(place='Tokyo', forecast_days=3))
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import plotly.express as px
+from backend import get_data
 
+# add title, text input, slider, select box, subheader
 st.title("Weather Forecast for the Next Days")
+
 place = st.text_input("Place: ").capitalize()
 
 days = st.slider("Forecast Days", min_value=1, max_value=5,
@@ -11,3 +15,23 @@ option = st.selectbox("Select data to view",
 
 st.subheader(f"{option} for the next {days} days in {place}")
 
+if place:
+    try:
+        # get the temperature/sky data
+        filtered_data = get_data(place, days)
+
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"] / 10 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            # create a temperature plot
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature"})
+            st.plotly_chart(figure)
+
+        if option == "Sky":
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            images = {"Clear": "images_weather/clear.png", "Clouds": "images_weather/cloud.png",
+                      "Snow": "images_weather/snow.png", "Rain": "images_weather/rain.png"}
+            image_paths = [images[condition] for condition in sky_conditions]
+            st.image(image_paths, width=115)
+    except KeyError:
+        st.write("That place does not exist.")
